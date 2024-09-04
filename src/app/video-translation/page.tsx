cf8c87283331be35e4c46fb796e5b7827856057c
humanshi123 <humanshi.auspicioussoft@gmail.com.com>
2024-09-04 17:51:44 +0530

    fixes 04-09

--- src/app/video-translation/page.tsx
+++ src/app/video-translation/page.tsx
@@ -1,17 +1,12 @@
-"use client";
-import AddVideo from "@/components/AddVideo";
-import AddVoice from "@/components/AddVoice";
-import Subtitles from "@/components/Subtitles";
-import React from "react";
+import Subtitles from '@/components/Subtitles';
+import React from 'react';
 
 const Page = () => {
-  return (
-    <div>
-      <AddVideo />
-      <AddVoice />
-      <Subtitles />
-    </div>
-  );
-};
+    return (
+        <div>
+            <Subtitles />
+        </div>
+    );
+}
 
 export default Page;
